1c19a99c88a1533d7bc6314a9e8fe38619ec8263
Mehmet Demircioglu <demircioglumhmt@gmail.com>
2022-02-14 14:18:49 +0300

    add use http and api

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,6 +1,40 @@
+import { useEffect } from "react";
+import NotQuotesFound from "../components/quotes/NotQuotesFound";
 import QuotesList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 
 const AllQuotues =() => {
+
+
+    const {sendRequest,status,data:loadedQuotes,error} = useHttp(
+        getAllQuotes,
+        true
+    )
+
+    useEffect(() => {
+        sendRequest();
+    },[sendRequest])
+
+    if(status === 'pending') {
+        return(
+            <div>
+                <LoadingSpinner />
+            </div>
+        )
+    }
+
+    if(error) {
+        return <p>{error}</p>
+    }
+
+    if(status === 'completed' && (!loadedQuotes || loadedQuotes.length ===0)){
+        return <NotQuotesFound />
+    }
+
+
+
 
     return <QuotesList />
 
